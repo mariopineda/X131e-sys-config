de18e8081c33058361628162697f38471d100bb6
Mario Pineda <mpineda@ualberta.ca>
2018-03-29 22:30:02 -0600

    Added file with installation script

---
+++ system-config.sh
@@ -0,0 +1,18 @@
+sudo apt-get update
+sudo apt-get upgrade
+sudo apt-get dist-upgrade
+sudo apt-get install vim 
+sudo apt-get install chromium-browser
+sudo apt-get install texlive-full
+sudo apt-get install texmaker
+sudo apt-get install mupdf
+sudo apt-get install default-jdk
+sudo apt-get install git
+sudo apt-get install vlc
+sudo apt-get install arduino
+sudo apt-get install fortune
+wget http://download.processing.org/processing-3.3.7-linux64.tgz
+sudo mv processing-3.3.7-linux64.tgz /opt
+cd /opt
+sudo tar xvf processing-3.3.7-linux64.tgz
+mkdir repos 
